4d64cf1ad95c9e054f2eb73e7ed6c2c22c373b07
huongctt-1149 <chu.thi.thanh.huong@sun-asterisk.com>
2022-03-07 07:32:15 +0700

    login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,29 +1,81 @@
-import { useState } from 'react';
+import { useState, useRef , useContext} from 'react';
 
+import AuthContext from '../../store/auth-context'
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext)
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true)
+    let url;
+    if(isLogin) {
+      url = 'login url'
+    } else {
+      url = 'signup url'
+    }
+    fetch(url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email:enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }).then(res => {
+        setIsLoading(false)
+        if(res.ok) {
+          return res.json();
+        } else {
+          return res.json().then(data => {
+            //show errror modal
+            console.log(data)
+            let errorMessage = "Unsuccessful!"
+            throw new Error(errorMessage);
+          })
+        }
+      }).then(data => {
+        console.log(data)
+        authCtx.login(data.idToken)
+      }).catch(err =>{
+        alert(err.message)
+      })
+
+  }
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordInputRef}/>
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending...</p>}
           <button
             type='button'
             className={classes.toggle}
